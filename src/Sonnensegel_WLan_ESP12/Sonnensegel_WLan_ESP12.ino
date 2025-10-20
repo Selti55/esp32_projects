b7071e01658fe2e2a67ae32f25d160669a017b1d
Werner <Selti55@users.noreply.github.com>
2025-10-20 11:34:02 +0200

    neu hinzugefügt

---
+++ src/Sonnensegel_WLan_ESP12/Sonnensegel_WLan_ESP12.ino
@@ -0,0 +1,109 @@
+/*
+   -- Sonnensegel_WLan_ESP12 --
+   
+   This source code of graphical user interface 
+   has been generated automatically by RemoteXY editor.
+   To compile this code using RemoteXY library 3.1.13 or later version 
+   download by link http://remotexy.com/en/library/
+   To connect using RemoteXY mobile app by link http://remotexy.com/en/download/                   
+     - for ANDROID 4.15.01 or later version;
+     - for iOS 1.12.1 or later version;
+    
+   This source code is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.    
+*/
+
+//////////////////////////////////////////////
+//        RemoteXY include library          //
+//////////////////////////////////////////////
+
+// you can enable debug logging to Serial at 115200
+//#define REMOTEXY__DEBUGLOG    
+
+// RemoteXY select connection mode and include library 
+#define REMOTEXY_MODE__WIFI_POINT
+
+#include <ESP8266WiFi.h>
+
+// RemoteXY connection settings 
+#define REMOTEXY_WIFI_SSID "RemoteXY"
+#define REMOTEXY_WIFI_PASSWORD ""
+#define REMOTEXY_SERVER_PORT 6377
+
+
+#include <RemoteXY.h>
+
+// RemoteXY GUI configuration  
+#pragma pack(push, 1)  
+uint8_t RemoteXY_CONF[] =   // 137 bytes
+  { 255,6,0,0,0,130,0,19,0,0,0,0,21,1,106,200,1,1,6,0,
+  1,5,9,92,21,2,2,31,77,111,116,111,114,32,49,32,97,117,102,0,
+  1,6,32,92,21,2,119,31,77,111,116,111,114,32,49,32,65,98,0,1,
+  6,62,89,21,2,39,31,77,111,116,111,114,32,50,32,65,117,102,0,1,
+  6,85,89,21,2,135,31,77,111,116,111,114,32,50,32,65,98,0,1,6,
+  115,89,21,2,1,31,77,111,116,111,114,32,51,32,65,117,102,0,1,7,
+  138,89,21,2,123,31,77,111,116,111,114,32,51,32,65,98,0 };
+  
+// this structure defines all the variables and events of your control interface 
+struct {
+
+    // input variables
+  uint8_t button_01; // =1 if button pressed, else =0
+  uint8_t button_02; // =1 if button pressed, else =0
+  uint8_t button_03; // =1 if button pressed, else =0
+  uint8_t button_04; // =1 if button pressed, else =0
+  uint8_t button_05; // =1 if button pressed, else =0
+  uint8_t button_06; // =1 if button pressed, else =0
+
+    // other variable
+  uint8_t connect_flag;  // =1 if wire connected, else =0
+
+} RemoteXY;   
+#pragma pack(pop)
+ 
+/////////////////////////////////////////////
+//           END RemoteXY include          //
+/////////////////////////////////////////////
+
+#define PIN_BUTTON_01 6
+#define PIN_BUTTON_02 7
+#define PIN_BUTTON_03 8
+#define PIN_BUTTON_04 15
+#define PIN_BUTTON_05 2
+#define PIN_BUTTON_06 0
+
+
+void setup() 
+{
+  RemoteXY_Init (); 
+  
+  pinMode (PIN_BUTTON_01, OUTPUT);
+  pinMode (PIN_BUTTON_02, OUTPUT);
+  pinMode (PIN_BUTTON_03, OUTPUT);
+  pinMode (PIN_BUTTON_04, OUTPUT);
+  pinMode (PIN_BUTTON_05, OUTPUT);
+  pinMode (PIN_BUTTON_06, OUTPUT);
+  
+  // TODO you setup code
+  
+}
+
+void loop() 
+{ 
+  RemoteXY_Handler ();
+  
+  digitalWrite(PIN_BUTTON_01, (RemoteXY.button_01==0)?LOW:HIGH);
+  digitalWrite(PIN_BUTTON_02, (RemoteXY.button_02==0)?LOW:HIGH);
+  digitalWrite(PIN_BUTTON_03, (RemoteXY.button_03==0)?LOW:HIGH);
+  digitalWrite(PIN_BUTTON_04, (RemoteXY.button_04==0)?LOW:HIGH);
+  digitalWrite(PIN_BUTTON_05, (RemoteXY.button_05==0)?LOW:HIGH);
+  digitalWrite(PIN_BUTTON_06, (RemoteXY.button_06==0)?LOW:HIGH);
+  
+  // TODO you loop code
+  // use the RemoteXY structure for data transfer
+  // do not call delay(), use instead RemoteXY_delay() 
+
+
+}
